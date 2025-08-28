70b69f752dd8a9514b80beaf17585887158e2a28
joseph-akintunde <57961001+joseph-akintunde@users.noreply.github.com>
2025-08-28 16:21:10 -0400

    Add files via upload

---
+++ Measures of central tendency.R
@@ -0,0 +1,33 @@
+
+R version 4.5.1 (2025-06-13) -- "Great Square Root"
+Copyright (C) 2025 The R Foundation for Statistical Computing
+Platform: aarch64-apple-darwin20
+
+R is free software and comes with ABSOLUTELY NO WARRANTY.
+You are welcome to redistribute it under certain conditions.
+Type 'license()' or 'licence()' for distribution details.
+
+  Natural language support but running in an English locale
+
+R is a collaborative project with many contributors.
+Type 'contributors()' for more information and
+'citation()' on how to cite R or R packages in publications.
+
+Type 'demo()' for some demos, 'help()' for on-line help, or
+'help.start()' for an HTML browser interface to help.
+Type 'q()' to quit R.
+
+[R.app GUI 1.82 (8536) aarch64-apple-darwin20]
+
+[Workspace restored from /Users/joseph/.RData]
+[History restored from /Users/joseph/.Rapp.history]
+
+> data <- c(1:50)
+> 
+> data
+ [1]  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 33 34 35 36 37 38 39 40 41 42 43 44 45 46 47 48 49 50
+> mean(data)
+[1] 25.5
+> median(data)
+[1] 25.5
+> 
