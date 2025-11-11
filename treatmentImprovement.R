b5e478ae2cfee178e62fbdc2a1d6a591ef3b8eea
joseph-akintunde <57961001+joseph-akintunde@users.noreply.github.com>
2025-11-11 15:56:11 -0500

    Add files via upload

---
+++ treatmentImprovement.R
@@ -0,0 +1,10 @@
+#Load dummy dataset
+data_frame <- read.csv("https://goo.gl/j6lRXD")
+table(data_frame$treatment, data_frame$improvement)
+#Apply chi-square function to check the difference between treatment and improvement
+chisq.test(data_frame$treatment, data_frame$improvement, correct=FALSE)
+Pearson's Chi-squared test
+
+data:  data_frame$treatment and data_frame$improvement
+X-squared = 5.5569, df = 1, p-value = 0.01841
+
